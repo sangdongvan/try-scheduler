4d8b514373fa3d85ce8c28a1c5700a895537da73
Sang Dong <sang.dongvan@asnet.com.vn>
2025-03-10 16:58:29 +0700

    sync

--- src/task/task.ts
+++ src/task/task.ts
@@ -9,17 +9,53 @@ export class MakeOrderImpl extends AbstractActor implements MakeOrder {
 
   async start() {
     await this.registerActorReminder(
-      "start",
+      "start1",
       Temporal.Duration.from({ seconds: 1 }),
-      Temporal.Duration.from({ seconds: 3 })
+      Temporal.Duration.from({ seconds: 3 }),
+      undefined,
+      "start1"
+    );
+
+    await this.registerActorReminder(
+      "start2",
+      Temporal.Duration.from({ seconds: 3 }),
+      Temporal.Duration.from({ seconds: 5 }),
+      undefined,
+      "start2"
+    );
+
+    await this.registerActorReminder(
+      "end",
+      Temporal.Duration.from({ seconds: 2 }),
+      Temporal.Duration.from({ seconds: 4 }),
+      undefined,
+      "end"
     );
   }
 
   /**
    * @override
    */
-  async receiveReminder() {
-    this.count = this.count + 1;
-    console.log("receive reminder " + this.count);
+  async receiveReminder(_data: string) {
+    if (_data == "start1") {
+      await this.unregisterActorReminder("start1");
+      this.count = this.count + 1;
+      console.log("receive reminder [START 1] " + this.count);
+      return;
+    }
+
+    if (_data == "start2") {
+      await this.unregisterActorReminder("start2");
+      this.count = this.count + 1;
+      console.log("receive reminder [START 2] " + this.count);
+      return;
+    }
+
+    if (_data == "end") {
+      await this.unregisterActorReminder("end");
+      this.count = this.count + 1;
+      console.log("receive reminder [END    ] " + this.count);
+      return;
+    }
   }
 }
